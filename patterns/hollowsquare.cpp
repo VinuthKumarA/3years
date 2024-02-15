0fbc9688e21f235ea5b31f1a0dfbf4b49399ea67
VinuthKumarA <vinuthkumar2002@gmail.com>
2024-02-15 23:42:53 +0530

    add files

---
+++ patterns/hollowsquare.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout <<"enter the order: ";cin>>a;
+    cout <<"enter the order: ";cin>>b;
+    for(int i=1;i<=a;i++){
+        for(int j=1;j<=b;j++){
+            if(i==1 || i==a ||j==1 ||j==b){
+                cout<<"*";
+            }else{
+                cout<<" ";
+            }
+        }cout<<endl;
+    }
+    return 0;
+
+}
