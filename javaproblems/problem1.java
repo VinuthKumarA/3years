29138989d8f2a668c9475119be342bc9cab2b2bf
VinuthKumarA <vinuthkumar2002@gmail.com>
2024-03-30 18:28:26 +0530

    uploaded Initial commit

--- javaproblems/problem1.java
+++ javaproblems/problem1.java
@@ -1,7 +1,19 @@
 package javaproblems;
+import java.util.Scanner;
 public class problem1 {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the cost price : ");
+        int cp = sc.nextInt();
+        System.out.println("enter the sell price : ");
+        int sp = sc.nextInt();
+        if(sp>cp){
+            System.out.println("profit"+" : "+(sp-cp));
+        } else if (sp<cp) {
+            System.out.println("loss"+" : "+(sp-cp));
+        }else{
+            System.out.println("no profit or loss");
+        }
     }
 
 }
