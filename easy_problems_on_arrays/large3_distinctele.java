567096358730e0148330a036580d003b75291118
VinuthKumarA <vinuthkumar2002@gmail.com>
2024-04-24 23:51:04 +0530

    uploaded Initial commit

---
+++ easy_problems_on_arrays/large3_distinctele.java
@@ -0,0 +1,10 @@
+package easy_problems_on_arrays;
+
+import java.util.Scanner;
+
+public class large3_distinctele {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter th");
+    }
+}
